0894f39fd2cfbed9b18ce0aee63d21f1573bc52c
mzelder <maciej.zelder@wp.pl>
2025-02-19 18:45:20 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿var w = Console.WindowWidth;
+var h = Console.WindowHeight;
+int positionX = w / 2;
+int positionY = h / 2;
+
+char appleEmote = '';
+char snakeHeadEmote = 'X'; 
+char snakeBodyEmote = 'O';
+
+ConsoleKeyInfo keyInfo;
+
+while (true) {
+     Initialize();
+     
+     keyInfo = Console.ReadKey();
+     switch (keyInfo.Key)
+     {
+        case ConsoleKey.UpArrow:
+            positionY--;
+            break;
+        case ConsoleKey.DownArrow:
+            positionY++;
+            break;
+        case ConsoleKey.LeftArrow:
+            positionX--;
+            break;
+        case ConsoleKey.RightArrow:
+            positionX++;
+            break;
+     }
+
+     UpdateUI();
+
+}
+
+void Initialize() {
+    Console.CursorVisible = false;
+    UpdateUI();
+}
+
+void UpdateUI() {
+    Console.Clear();
+    Console.SetCursorPosition(positionX, positionY);
+    Console.Write(snakeHeadEmote);
+    for (int i = 0; i < 10; i++) { Console.Write(snakeBodyEmote); }
+}
